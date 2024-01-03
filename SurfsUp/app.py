ea24f4613d9516ddd3460a28244c92e69af7325e
DanielHughes1580 <rayman_dan@hotmail.co.uk>
2024-01-03 22:57:04 +0000

    Cleaned up app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,6 +1,4 @@
-# Import dependencies and packages
-from statistics import mean
-import numpy as np
+# Import the dependencies.
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -8,40 +6,49 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 from datetime import datetime, timedelta
 
-# Engine Creation
+#################################################
+# Database Setup
+#################################################
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-
-# Remodel existing database and refrence tables
+# Reflect an existing database into a new model
 Base = automap_base()
+# Reflect the tables
 Base.prepare(autoload_with=engine)
 print(Base.classes.keys())
+# Save references to each table
 measure = Base.classes.measurement
 station_ref = Base.classes.station
-
-# Create a session link
+# Create our session (link) from Python to the DB
 session = Session(engine)
 
+#################################################
 # Flask Setup
+#################################################
 app = Flask(__name__)
 
-# Routes
+#################################################
+# Flask Routes
+#################################################
 @app.route("/")
 def welcome():
-    """List all available api routes."""
+    """All available api routes."""
     return (
         f"Available Routes:<br/>"
+        f"Route containing percipitation analysis:"
         f"/api/v1.0/precipitation<br/>"
+        f"Route containing a list of all the stations:"
         f"/api/v1.0/stations<br/>"
+        f"Route containing temperature observations from the most active station within the previous year:"
         f"/api/v1.0/tobs<br/>"
+        f"Route to see the minimum, maximum and average temperature recorded for any time after a specific date or within a certain time period:"
         f"/api/v1.0/start_date<br/>"
         f"/api/v1.0/start_date/end_date<br/>"
-        f"Start and end date should be written in the following format"
+        f"start_date and end_date should be replaced with the date you are seeking written in the following format: Day-Month-Year"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
-
     year_ago = datetime(2017, 8, 23) - timedelta(days=365)
     prcp_results = session.query(measure.date, measure.prcp).filter(measure.date >= year_ago).all()
     session.close()
@@ -55,15 +62,10 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
-  
     stat_results = session.query(station_ref.name).all()
-
     session.close()
-
     all_stations = list(np.ravel(stat_results))
-
     return jsonify(all_stations)
-
 
 @app.route("/api/v1.0/tobs")
 def tobs():
@@ -99,7 +101,7 @@ def stats(start = None, end = None):
     se_stat = list(np.ravel(res))
     return jsonify(se_stat)
 
-# Debug
+# Debugging
 if __name__ == '__main__':
     app.run(debug=True)
 
